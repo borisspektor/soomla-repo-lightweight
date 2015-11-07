6ffbb19e7d4eb871f2f9ed14c61f12aa0333408e
Eugene Butusov <EvgenyButusov@gmail.com>
2015-11-07 11:08:13 +0300

    Add missed files for Unity3D-Store

--- Unity3D/unity3d-store-prune.sh
+++ Unity3D/unity3d-store-prune.sh
@@ -5,22 +5,26 @@ FILES=(
     Soomla/Assets/Plugins/iOS/libSoomlaIOSStore.a
     Soomla/Assets/Soomla/compilations
     soomla-unity3d-core.unitypackage
-    deploy/out
+    deploy/out/
     soomla-unity3d-store_release-v1.5.2.unitypackage
-    soomla-native/ios
-    unity3.5
+    unity3.5/
     Soomla/Assets/Plugins/iOS/libSoomlaIOSCore.a
-    unity4.0
+    Soomla/Assets/Plugins/Android/square-otto-1.3.2.jar
+    Soomla/Assets/Plugins/Android/SoomlaAndroidCore.jar
+    unity4.0/
     Soomla/Assets/Plugins/iOS/libUnityiOSStore.a
+    Soomla/Assets/Plugins/Android/AndroidStoreGooglePlay.jar
+    Soomla/Assets/Plugins/Android/UnityAndroidStore.jar
     Soomla/Assets/Plugins/Newtonsoft.Json.dll
     Soomla/Assets/Plugins/WP8/Newtonsoft.Json.dll
-    soomla-native/compilations
-    soomla-native/android
+    soomla-native/compilations/
+    soomla-native/android/
+    soomla-native/ios/
     soomla-native/projects/unity-wp8-fake-assembly/wp-store/packages/Newtonsoft.Json.6.0.4
     Soomla/Assets/Plugins/Android/AndroidStore.jar
     Soomla/Assets/Plugins/Android/Soomla.jar
     soomla-native/projects/ios-store/build
-    src
+    src/
     Soomla/Assets/Plugins/WP8/wp-store.dll
     Soomla/Assets/Plugins/WP8/soomla-wp-core.dll
 )
