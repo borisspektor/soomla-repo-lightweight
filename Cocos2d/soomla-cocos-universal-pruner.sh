af822edab6917b2132d9fd882f06617b67630be1
Eugene Butusov <EvgenyButusov@gmail.com>
2015-11-07 13:06:46 +0300

    Process all Cocos2d-x SOOOMLA modules

---
+++ Cocos2d/soomla-cocos-universal-pruner.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+FILES=(
+     build/
+     soomla-native/
+     development/soomla-native/projects/cocos2dx-ios-core/build
+     development/soomla-native/projects/cocos2dx-android-core/build
+
+     development/soomla-native/projects/cocos2dx-ios-store/build
+     development/soomla-native/projects/cocos2dx-android-store/build
+     cocos2dx-store
+
+     developement/soomla-native/projects/cocos2dx-ios-profile/build
+     developement/soomla-native/projects/cocos2dx-android-profile/build
+     development/soomla-native/projects/cocos2dx-ios-profile/build
+     development/soomla-native/projects/cocos2dx-android-profile/build
+
+     development/testing
+)
+
+./pruner.sh ${FILES[@]}
