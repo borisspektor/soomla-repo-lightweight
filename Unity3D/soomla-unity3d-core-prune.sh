07663b12176e651bc27494937fbe25afa0a15d22
Eugene Butusov <EvgenyButusov@gmail.com>
2015-11-07 10:58:04 +0300

    Add missed files for Unity3D-Core

--- Unity3D/soomla-unity3d-core-prune.sh
+++ Unity3D/soomla-unity3d-core-prune.sh
@@ -4,6 +4,9 @@ FILES=(
 	soomla-unity3d-core.unitypackage 
 	deploy/out/soomla-unity3d-core.unitypackage
     Soomla/Assets/Plugins/iOS/libUnitySoomlaiOSCore.a
+    Soomla/Assets/Plugins/iOS/libSoomlaIOSCore.a
+    Soomla/Assets/Plugins/Android/square-otto-1.3.2.jar
+    Soomla/Assets/Plugins/Android/SoomlaAndroidCore.jar
     Soomla/Assets/Plugins/Android/UnitySoomlaAndroidCore.jar
 )
 
