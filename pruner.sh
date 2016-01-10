bb13c8e8ef11d4c01a8a7aa5706ccebadafd10d4
Eugene Butusov <EvgenyButusov@gmail.com>
2016-01-10 19:58:09 +0300

    Polish submodule updating code

--- pruner.sh
+++ pruner.sh
@@ -20,16 +20,14 @@ rm -rf .git/refs/original/
 git reflog expire --expire=now --all
 git gc --aggressive --prune=now
 
-
+echo "Updating submodules..."
 git submodule init
 git submodule update
 for submodule_folder in `find ./submodules -type d -maxdepth 1`
 do
         cd $submodule_folder
-        pwd
         commit_hash_raw="$(git log origin/master | head -1)"
         commit_hash_splitted=($commit_hash_raw)
-        echo ${commit_hash_splitted[1]}
         git checkout ${commit_hash_splitted[1]}
         cd -
         git add $submodule_folder
